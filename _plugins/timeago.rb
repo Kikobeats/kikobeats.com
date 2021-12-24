18106452865277d5aba9eaff70a4d69592b183ab
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-12-24 15:32:43 +0000

    chore: shows today under 0 days of publication

--- _plugins/timeago.rb
+++ _plugins/timeago.rb
@@ -2,7 +2,7 @@ module Jekyll
   module TimeAgo
     def timeago(date)
       n = (DateTime.now - Date.parse(date.to_s)).to_i
-      "#{n} days ago"
+      n > 0 ? "#{n} days ago" : "today"
     end
   end
 end
